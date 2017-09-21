90ab93a6dfb714ba2f61b68d25eea5464471e8c8
Mark Dessain <mark.dessain@simplybusiness.co.uk>
2017-09-21 14:48:21 +0100

    Adding proto and Postgres

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,22 @@
 FROM golang:1.9.0-alpine3.6
 
 RUN apk add --no-cache git
+RUN apk add --no-cache openssl
+RUN apk add --no-cache protobuf
 
-RUN go get -d -v github.com/kniren/gota/dataframe
+RUN go get gopkg.in/kniren/gota.v0/dataframe
+RUN go get github.com/golang/protobuf/protoc-gen-go
+RUN go get github.com/lib/pq
 
 WORKDIR /go
-COPY ./src .
+COPY . .
 
-RUN go build -o app .
+RUN protoc --go_out=plugins=grpc:src/ proto/*.proto
+
+RUN go build -o app ./src
 
 FROM alpine:latest
-RUN apk --no-cache add ca-certificates
+#scratch
 WORKDIR /root/
 COPY --from=0 /go .
 CMD ["./app"]
--- src/main.go
+++ src/main.go
@@ -5,8 +5,30 @@ import "fmt"
 import "github.com/kniren/gota/dataframe"
 import "github.com/kniren/gota/series"
 
+// import "database/sql"
+// import _ "github.com/lib/pq"
+
+import pb "proto"
+
+// const (
+//   host     = "localhost"
+//   port     = 5432
+//   user     = "mark.dessain"
+//   password = ""
+//   dbname   = "call_me_maybe_development"
+// )
+
 func main() {
     fmt.Println("hello world")
+
+    p := pb.Person{
+      Id:    1234,
+      Name:  "John Doe",
+      Email: "jdoe@example.com",
+      Phones: []*pb.Person_PhoneNumber{
+        {Number: "555-4321", Type: pb.Person_HOME},
+      },
+    }
 
     df := dataframe.New(
     	series.New([]string{"b", "a"}, series.String, "COL.1"),
@@ -14,5 +36,25 @@ func main() {
     	series.New([]float64{3.0, 4.0}, series.Float, "COL.3"),
     )
 
+    // psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+    //
+    // db, err := sql.Open("postgres", psqlInfo)
+    // if err != nil {
+  	// 	fmt.Println(err)
+  	// }
+    //
+    // err = db.Ping()
+    // if err != nil {
+    //   panic(err)
+    // }
+    //
+  	// rows, err := db.Query("SELECT full_name FROM dialler_leads")
+    // if err != nil {
+  	// 	fmt.Println(err)
+  	// }
+
+    fmt.Println(p)
     fmt.Println(df)
+    // fmt.Println(db)
+    // fmt.Println(rows)
 }
